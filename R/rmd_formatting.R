26fe597a3d8f47e92469d20d072667bbd5715d13
Vilmantas Gegzna <GegznaV@gmail.com>
2019-07-31 17:25:02 +0300

    Update rmd_formatting.R

--- R/rmd_formatting.R
+++ R/rmd_formatting.R
@@ -184,13 +184,13 @@ CAPTION = "" # Figure caption/description.
     if (zero_selection) {
         # Place cursor inside parentheses of `include_graphics`
         pos <- rstudioapi::document_position(
-            row    = rs_get_first_selected_row_index(context = context) + 1,
+            row    = rs_get_index_first_selected_row(context = context) + 1,
             column = 26
         )
     } else {
         # Place cursor inside parentheses of `CAPTION`
         pos <- rstudioapi::document_position(
-            row    = rs_get_first_selected_row_index(context = context) + 3,
+            row    = rs_get_index_first_selected_row(context = context) + 3,
             column = 12
         )
     }
