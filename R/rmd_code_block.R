67a7854482ea59a68f165a51ef86e6860b3bac6f
Vilmantas Gėgžna <GegznaV@gmail.com>
2022-03-27 10:47:07 +0300

    Make more MVE friendly code
    MVE – markdown visual editor

--- R/rmd_code_block.R
+++ R/rmd_code_block.R
@@ -4,8 +4,15 @@
 #' @title Convert rows into the block of code.
 #' @description
 #' RStudio add-in to insert selected lines into code block: \itemize{
-#'    \item \code{rmd_code_block()} - verbatim code block;
+#'    \item \code{rmd_code_block()} - verbatim code block.
+#'          **Note:** in Markdown Visual Editor mode does not work.
 #'    \item \code{rmd_code_block_r()} - R code block.
+#'          **Note!** In Markdown Visual Editor (MVE) mode works differently:
+#'          in MVE mode, internal RStudio API function `insertChunk` is
+#'          executed, which means that currently non-selected text will not be
+#'          included in the chunk automatically. If the chunk is included as
+#'          the last line of the document in MVE mode, the selected text is
+#'          interpreted as chunk options, otherwise as chunk contents.
 #' }.
 #' \code{rmd_code_block_r_split()} - splits block of R code.
 #'
@@ -13,6 +20,8 @@
 #' These functions internally use function
 #' \code{\link[addin.tools]{rs_enclose_selected_rows_with}()} -
 #' that adds lines above and below the selection.
+#'
+#' @importFrom addin.tools rs_get_context is_rmd_visual_mode
 #'
 #' @inheritParams addin.tools::rs_get_index
 #'
@@ -23,29 +32,44 @@ NULL
 #' @rdname rmd_code_block
 #' @export
 rmd_code_block_r <- function(context = rs_get_context()) {
-  rs_enclose_selected_rows_with(
-    text_above = "```{r}",
-    text_below = "```",
-    context = context
-  )
+  if (is_rmd_visual_mode()) {
+    id <- context$id
+    text <- rstudioapi::selectionGet(id = id)$value
+    rstudioapi::executeCommand("insertChunk", quiet = TRUE)
+    rstudioapi::selectionSet(text, id = id)
+  } else {
+    rs_enclose_selected_rows_with(
+      text_above = "```{r}",
+      text_below = "```",
+      context = context
+    )
+  }
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname rmd_code_block
 #' @export
 rmd_code_block <- function(context = rs_get_context()) {
-  rs_enclose_selected_rows_with(
-    text_above = "```",
-    text_below = "```",
-    context = context
-  )
+  if (is_rmd_visual_mode()) {
+    stop("This addin does not work in Markdown visual editor mode")
+  } else {
+    rs_enclose_selected_rows_with(
+      text_above = "```",
+      text_below = "```",
+      context = context
+    )
+  }
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname rmd_code_block
 #' @export
 rmd_code_block_r_split <- function(context = rs_get_context()) {
-  rs_enclose_selected_rows_with(
-    text_above = "```\n\n```{r}",
-    context = context
-  )
+  if (is_rmd_visual_mode()) {
+    stop("This addin does not work in Markdown visual editor mode")
+  } else {
+    rs_enclose_selected_rows_with(
+      text_above = "```\n\n```{r}",
+      context = context
+    )
+  }
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
