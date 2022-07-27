8a3820b1d8fef5abf09398abb2a85b922d48d79f
Vilmantas Gėgžna <GegznaV@gmail.com>
2022-07-27 22:15:55 +0300

    Update addins.rmd-package.R

--- R/addins.rmd-package.R
+++ R/addins.rmd-package.R
@@ -1,29 +1,4 @@
-#' @name addins.rmd
-#' @docType package
-#'
-#' @title RStudio Add-ins for R Markdown
-#'
-#' @description
-#'
-#' `addins.rmd` provides a set of RStudio add-ins to format R Markdown documents.
-#'
-#' `License:` MIT   \cr
-#' `URL:` <https://github.com/GegznaV/addins.rmd> \cr
-#' `Bug reports and suggestions:` <https://github.com/GegznaV/addins.rmd/issues> \cr
-#' `Author:` Vilmantas Gegzna
-#'
-#' @seealso
-#'
-#' Use the links below to learn more about RStudio add-ins
-#' and especially how to use them in combination with user-defined
-#' keyboard shortcuts:
-#'
-#' 1. [RStudio add-ins](https://rstudio.github.io/rstudioaddins/);
-#' 2. [Keyboard shortcuts](https://rstudio.github.io/rstudioaddins/#keyboard-shorcuts);
-#' 3. [Customizing keyboard shortcuts.](https://support.rstudio.com/hc/en-us/articles/206382178-Customizing-Keyboard-Shortcuts)
-#'
-#'
-#'
+#' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
